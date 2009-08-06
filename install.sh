ad9b0f925bf6e0056cf5dd301cdc034b9d6caf76
iiPing <dbx0001@gmail.com>
2009-08-07 06:26:02 +0800

    modified: README added install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#version 0.01 install script
+mkdir -p ~/.gnome2/rhythmbox/plugins/rbskypemoodnotify
+cp *.py ~/.gnome2/rhythmbox/plugins/rbskypemoodnotify
+cp *.rb-plugin ~/.gnome2/rhythmbox/plugins/rbskypemoodnotify
+
+
+
